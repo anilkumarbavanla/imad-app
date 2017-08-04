3d94a7499fb5f559de59a141e02a508865f35868
anilkumarbavanla <30653623+anilkumarbavanla@users.noreply.github.com>
2017-08-04 19:11:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,8 +8,10 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get(('article-one',function(req,res)
-{res.send('Article-one requested and served here);});
+app.get('/article', function (req, res) {
+  res.send('/Article-one requested and served here'));
+});
+
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
