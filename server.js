d348a76d65616eb3dd99bb8183434a0c52bcdd56
anilkumarbavanla <30653623+anilkumarbavanla@users.noreply.github.com>
2017-08-04 20:51:50 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,25 +8,53 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+var articleone= {
+    title: 'Article-one I Anil',
+    heading: 'Article-one',
+    date: 'aug 4,2017',
+    content: "True friendship is like sound health; the value of it is seldom known until it be lost."
+};
+function createtemplate(data) {
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var content = data.content;
+var htmlTemplate = `
+    <html>
+    <head>
+        <title>
+            ${title}
+        <title>
+         <link href="/ui/style.css" rel="stylesheet" />
+     </head>
+       <body>
+        <div>
+            <a href='/'>
+                HOME
+            </a>
+        </div>
+        <h3>
+            ${heading}
+        </h3>
+        <div>
+            ${date}
+        </div>
+        <div>
+            <p>
+               ${content}
+            </p>
+        </div>
+    </body>
+</html>
+ `;
+ return htmlTemplate;
+}
 app.get('/article-one', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+  res.send(createTemplate(articleone));
 });
   app.get('/article-two', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
 });
-app.get('/article-one', function (req, res) {
-  res.send('Article-one requested and served here');
-});
-app.get('/article-two', function (req, res) {
-  res.send('Article-two requested and served here');
-});
-
-app.get('/article-three', function (req, res) {
-  res.send('Article-three requested and served here');
-});
-
-
-
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
