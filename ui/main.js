281fbdae02dac309aa2ef53b0c303688feff0f81
anilkumarbavanla <30653623+anilkumarbavanla@users.noreply.github.com>
2017-08-05 16:42:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,5 +6,5 @@ element.innerHTML='New Value';
 
 var img=document.getElementById('madi');
 img.onclick=function(){
-   img.style.marginLeft='100px';
+   var interval = setInterval(marginLeft,100);
 };
