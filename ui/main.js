76cf3f0b81234392ec0594febcfbab69b9844e7c
anilkumarbavanla <30653623+anilkumarbavanla@users.noreply.github.com>
2017-08-08 17:13:03 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,10 +1,16 @@
 console.log('Loaded!');
 
 var button=document.getElementById('counter');
-var counter = 0;
-button.onclick = function(){
+var request = new XMLHttpRequest();
 
-counter = counter + 1;
-var span =document.getElementById('count');
-span.innerHTML = counter.toString();
+request.onreadystatechange = function(){
+    if(request.readyState === XMLHttpRequest.Done ) {
+        if(request.status === 200){
+            var counter = request.responsText;
+            var span =document.getElementById('count');
+            span.innerHTML = counter.toString();
+        }
+    }
+    request.open('GET','http://bavanlaanilkumar007.imad.hasura-app.io/',true);
+    request.send(null);
 };
