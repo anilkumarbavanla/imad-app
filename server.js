41c25d0bed348866ed43661c491112a03ba21220
anilkumarbavanla <30653623+anilkumarbavanla@users.noreply.github.com>
2017-08-08 16:46:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -61,6 +61,12 @@ app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 
+app.get('/counter',function(req,res){
+   counter = counter+1;
+   res.send(counter.toString());
+});
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
